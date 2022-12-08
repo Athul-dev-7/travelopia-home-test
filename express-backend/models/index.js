70bb6cab9b6d752ae0a79dfb9073d9e9bfd7f1d2
Das <athuldas@hamon.in>
2022-12-08 15:15:22 +0530

    Updates models/index.js.

--- express-backend/models/index.js
+++ express-backend/models/index.js
@@ -8,7 +8,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const db = require('../models');
 let sequelize;
-sequelize = new Sequelize(process.env.DATABASE_URL, { logging: false });
+sequelize = new Sequelize(process.env.DATABASE_URL, { logging: true });
 
 fs.readdirSync(__dirname)
     .filter((file) => {
