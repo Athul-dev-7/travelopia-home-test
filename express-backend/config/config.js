808a656f7cc9fe375df16aba91709175b8d5fccc
Das <athuldas@hamon.in>
2022-12-08 16:55:13 +0530

    Updates config file.

--- express-backend/config/config.js
+++ express-backend/config/config.js
@@ -1,8 +1,18 @@
 require('dotenv').config();
 const { DATABASE_URL } = process.env;
 
-module.export = {
+module.exports = {
     development: {
+        url: DATABASE_URL,
+        dialect: 'postgres',
+        logging: false,
+    },
+    test: {
+        url: DATABASE_URL,
+        dialect: 'postgres',
+        logging: false,
+    },
+    production: {
         url: DATABASE_URL,
         dialect: 'postgres',
         logging: false,
