e35ac2ed425b739e61f1e07a5c4cd6ec07422418
Das <athuldas@hamon.in>
2022-12-10 15:50:03 +0530

    Adds error msg for handling empty-bookings.

--- react-frontend/src/pages/bookingList/bookingsList.jsx
+++ react-frontend/src/pages/bookingList/bookingsList.jsx
@@ -16,32 +16,37 @@ const BookingsList = () => {
     return (
         <div className="container">
             <h1 className="title">Booking List</h1>
-            <div className={styles['table-container']}>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>FirstName</th>
-                            <th>LastName</th>
-                            <th>Email</th>
-                            <th>CountryToVisit</th>
-                            <th>numberOfTravellers</th>
-                            <th>BudgetPerPerson</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.map((val) => (
-                            <tr key={val.uuid}>
-                                <td>{val.firstName}</td>
-                                <td>{val.lastName}</td>
-                                <td>{val.email}</td>
-                                <td>{val.countryToVisit}</td>
-                                <td>{val.numberOfTravellers}</td>
-                                <td>{val.budgetPerPerson}</td>
+            {data.length ? (
+                <div className={styles['table-container']}>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>FirstName</th>
+                                <th>LastName</th>
+                                <th>Email</th>
+                                <th>CountryToVisit</th>
+                                <th>numberOfTravellers</th>
+                                <th>BudgetPerPerson</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+                        </thead>
+                        <tbody>
+                            {data.map((val) => (
+                                <tr key={val.uuid}>
+                                    <td>{val.firstName}</td>
+                                    <td>{val.lastName}</td>
+                                    <td>{val.email}</td>
+                                    <td>{val.countryToVisit}</td>
+                                    <td>{val.numberOfTravellers}</td>
+                                    <td>{val.budgetPerPerson}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            ) : (
+                'There are no bookings'
+            )}
+
             <Link to="/" className={styles.link}>
                 Back to Booking Form
             </Link>
