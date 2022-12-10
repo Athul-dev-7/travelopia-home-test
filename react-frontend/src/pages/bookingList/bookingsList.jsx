d79b0f728dc8c155ece2f004aac2419dd1ed1436
Das <athuldas@hamon.in>
2022-12-10 14:05:04 +0530

    Adds table for showing the form submissions.

--- react-frontend/src/pages/bookingList/bookingsList.jsx
+++ react-frontend/src/pages/bookingList/bookingsList.jsx
@@ -1,105 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './bookingsList.module.css';
 import { Link } from 'react-router-dom';
+import { getAllBookings } from '../../service/api';
 
 const BookingsList = () => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        getAllBookings().then((resp) => {
+            if (resp.status === 200) {
+                setData(resp.data.data);
+            }
+        });
+    }, []);
+
     return (
         <div className={styles.container}>
             <h1>Booking List</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>FirstName</th>
-                        <th>LastName</th>
-                        <th>Email</th>
-                        <th>CountryToVisit</th>
-                        <th>numberOfTravellers</th>
-                        <th>BudgetPerPerson</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Athul</td>
-                        <td>Das</td>
-                        <td>athuldas@gmail.com</td>
-                        <td>India</td>
-                        <td>2</td>
-                        <td>$1000</td>
-                    </tr>
-                    <tr>
-                        <td>Athul</td>
-                        <td>Das</td>
-                        <td>athuldas@gmail.com</td>
-                        <td>India</td>
-                        <td>2</td>
-                        <td>$1000</td>
-                    </tr>
-                    <tr>
-                        <td>Athul</td>
-                        <td>Das</td>
-                        <td>athuldas@gmail.com</td>
-                        <td>India</td>
-                        <td>2</td>
-                        <td>$1000</td>
-                    </tr>
-                    <tr>
-                        <td>Athul</td>
-                        <td>Das</td>
-                        <td>athuldas@gmail.com</td>
-                        <td>India</td>
-                        <td>2</td>
-                        <td>$1000</td>
-                    </tr>
-                    <tr>
-                        <td>Athul</td>
-                        <td>Das</td>
-                        <td>athuldas@gmail.com</td>
-                        <td>India</td>
-                        <td>2</td>
-                        <td>$1000</td>
-                    </tr>
-                    <tr>
-                        <td>Athul</td>
-                        <td>Das</td>
-                        <td>athuldas@gmail.com</td>
-                        <td>India</td>
-                        <td>2</td>
-                        <td>$1000</td>
-                    </tr>
-                    <tr>
-                        <td>Athul</td>
-                        <td>Das</td>
-                        <td>athuldas@gmail.com</td>
-                        <td>India</td>
-                        <td>2</td>
-                        <td>$1000</td>
-                    </tr>
-                    <tr>
-                        <td>Athul</td>
-                        <td>Das</td>
-                        <td>athuldas@gmail.com</td>
-                        <td>India</td>
-                        <td>2</td>
-                        <td>$1000</td>
-                    </tr>
-                    <tr>
-                        <td>Athul</td>
-                        <td>Das</td>
-                        <td>athuldas@gmail.com</td>
-                        <td>India</td>
-                        <td>2</td>
-                        <td>$1000</td>
-                    </tr>
-                    <tr>
-                        <td>Athul</td>
-                        <td>Das</td>
-                        <td>athuldas@gmail.com</td>
-                        <td>India</td>
-                        <td>2</td>
-                        <td>$1000</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className={styles['table-container']}>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>FirstName</th>
+                            <th>LastName</th>
+                            <th>Email</th>
+                            <th>CountryToVisit</th>
+                            <th>numberOfTravellers</th>
+                            <th>BudgetPerPerson</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((val) => (
+                            <tr key={val.uuid}>
+                                <td>{val.firstName}</td>
+                                <td>{val.lastName}</td>
+                                <td>{val.email}</td>
+                                <td>{val.countryToVisit}</td>
+                                <td>{val.numberOfTravellers}</td>
+                                <td>{val.budgetPerPerson}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
             <Link to="/" className={styles.link}>
                 Back to Booking Form
             </Link>
